d52acb9a95e3312351beda59d87b4600c0afc28a
Alansiew <siewieraa@gmail.com>
2022-01-19 19:03:22 +0100

    changes

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,5 +1,17 @@
+import django_filters
 from django import forms
+
 from .models import Expense
+
+
+class ExpenseCategorySearchForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ('category',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].required = False
 
 
 class ExpenseSearchForm(forms.ModelForm):
@@ -10,3 +22,32 @@ class ExpenseSearchForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['name'].required = False
+
+
+class ExpenseAmountSearchForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ('amount',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['amount'].required = False
+
+
+class ExpenseDateSearchForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ('date',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['date'].input_formats = ['%Y-%m-%d','%Y.%m.%d','%Y/%m/%d','%d-%m-%Y','%d.%m.%Y','%d/%m/%Y']
+        self.fields['date'].required = False
+
+
+# Data searcher method
+
+class ExpenseFilter(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ('category',)
--- expenses/reports.py
+++ expenses/reports.py
@@ -1,8 +1,10 @@
 from collections import OrderedDict
 
-from django.db.models import Sum, Value
-from django.db.models.functions import Coalesce
+from django.db.models import Sum, Value, Count
+from django.db.models.functions import Coalesce, Cast, Extract
+from django.forms import models
 
+from .models import Expense
 
 def summary_per_category(queryset):
     return OrderedDict(sorted(
@@ -13,4 +15,16 @@ def summary_per_category(queryset):
         .annotate(s=Sum('amount'))
         .values_list('category_name', 's')
     ))
+
+def summary_per_id(queryset):
+    return OrderedDict(sorted(
+        queryset
+        .annotate(category_name=Count('name'))
+        .order_by()
+        .values('category_name')
+        .annotate(s=Sum('amount'))
+        .values_list('category_name', 's')
+    ))
+
+
 
